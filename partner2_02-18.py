a2479d417cd954ef4888b4cc0567b83f610b9ba3
Marco2-7 <mp9565105067@gmail.com>
2026-02-18 18:05:18 +0000

    Co-authored-by: NeoLuigi88 <NeoLuigi88@users.noreply.github.com>

---
+++ partner2_02-18.py
@@ -0,0 +1,22 @@
+print("One Piece is not too long just trust the process")
+
+print("Pizza is my favorite food!")
+print("45")
+name = "Marco"
+print(name)
+name = "Luis"
+num = 40
+age=20
+print("Marco's age is ", age)
+
+age=22
+print("Marco will be ", age, " in two years")
+print("What is your name?")
+name1 = input()
+print("Your name is ", name1)
+
+print("Pick a number ")
+num1=input()
+print("You chose your number as ", num1)
+
+print("Goodbye, Thank You")
