fefc34a45bce8c5c92e3bc1f601e9fea30a1c34c
Marco2-7 <mp9565105067@gmail.com>
2026-02-18 18:05:38 +0000

    commit

---
+++ 1101_2026/inclass.codepractice.py
@@ -0,0 +1,33 @@
+# Example 1: Create a variable and assign a value
+name="Marco"
+
+# Example 2: Create another variable with a number
+num= 4
+
+# Example 3: Print text an the screen
+print("Hello, this will print on screen")
+
+# Example 4: Print what the variable 'name' has inside
+print(name)
+
+# Example 5: Print a sentence using the variable 'name'
+print("my name is ", name)
+
+# Example 6: Change the value of the variable and print it again
+name="Mp"
+print("My nickname is", name )
+
+# Example 7: Ask user what their favorite animal is
+print("What is your favorite animal?")
+animal=input()
+print("I didn't know that a ", animal, " was your favorite animal")
+
+# Example 8: Use the variable create in Example 2
+# If the variable is greater than 10 print a message
+# Otherwise, print another message
+
+if num>10:
+    print("Congrats, your number is greater than 10")
+
+else:
+    print("Sorry, your number is not greater than 10")
